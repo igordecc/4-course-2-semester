1ec210bdc396832a6957ea301e01d1aff8447b22
igordecc <Igordecc@yandex.ru>
2018-05-11 23:43:05 +0400

    Add 4b

---
+++ Din.system practice/4b/4id.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def logistic_map(x0, a, b):
+    return a - b*x0 + x0**3
+
+def list_creator(x0, a, b, idepth):
+    xlist = [logistic_map(x0, a, b)]
+    for i in range(idepth):
+        xlist.append(logistic_map(xlist[-1], a, b))
+        xlist.append(xlist[-1])
+    return xlist
+
+
+def plotlogmap(x0, a, b, idepth):
+
+
+    x = np.linspace(-1.5, 1.5, 100)
+    plt.plot(x, x, label="x(n+1) = x(n)")
+    plt.plot(x, logistic_map(x, a, b), label='x(n+1) = a - b*x(n) - x(n)**3')
+
+
+    xlist = list_creator( x0, a, b, idepth )
+    ylist = [xlist[0]]+xlist[:-1]
+
+    for i in range(len(xlist)-1):
+        line = plt.plot( [ylist[i], ylist[i + 1]], [xlist[i], xlist[i + 1]], color="red")
+
+    plt.xlabel('x_n')
+    plt.ylabel('x_n+1')
+    plt.title("One-Dimensional Mapping")
+    #plt.legend()
+    plt.show()
+
+if __name__=="__main__":
+    a = .5     #float(input("a: "))
+    b = 1
+    idepth = 100    #int(input("depth: "))
+    x0 = 0
+    plotlogmap(x0, a, b, idepth)
---
+++ Din.system practice/4b/4map.py
@@ -0,0 +1,79 @@
+from matplotlib import pyplot as plt
+from matplotlib import transforms as trns
+import numpy as np
+
+#карта динамических режимов
+
+def VDP_eq(x, y, lambd, a):
+    return (lambd - x**2)*y - a - (x + 1)**2
+
+def VDP_eq_x(x, y, lambd, a):
+    return y
+
+def euler_method(f, x, y, h):
+    return y + h*(f(x, y))
+
+#TODO there is example in 1.py for solving dual variable equasion
+#TODO 1 connect VDP1 and VDP2 under the general VDP function
+#TODO write euler method for this "vector" function.
+
+def final_point(x0, a, b, additional_points=100, N=1000):
+    for i in range(N-additional_points):
+        x0 = VDP_eq(x0, a, b)
+
+    x0_additional_points = []
+    for i in range(additional_points):
+        x0 = VDP_eq(x0, a, b)
+        x0_additional_points.append(x0)
+
+    return x0_additional_points
+
+
+def make_array(x0, a, b, ap):
+    a = np.arange(*a, dtype=np.float64)
+    b = np.arange(*b, dtype=np.float64)
+    abmap = [ [final_point(x0, i, j, ap) for j in b] for i in a]
+    return np.array(abmap) #.shape((a.__len__(), b.__len__(), -1))
+
+def interpret_map(abmap):
+    colormap = [[len(set(j)) for j in i] for i in abmap]
+    #abmap = [set(j, 4) for j in for i in abmap] # remove duplicates
+    #colormap = len()
+    return colormap
+
+#TODO 1 read how to make color map, please (matplotlib)
+#TODO СТЯНИ ПЕРЕД РАБОТОЙ
+#TODO 2 find "кубическое уравнение" in a book
+
+#todo 1a make a,b axies
+#todo 1b make collor conture map ///////
+#todo 1bb manage collor conture map ///////
+#todo 1c turn the map to 90 degrees
+
+
+
+if __name__=="__main__":
+    x0 = 0
+    a = (-0.6, 0.6, 0.04)
+    b = (0.8, 2.5, 0.04)
+    abmap = make_array(x0, a, b, ap=20)
+    colormap = interpret_map(abmap)
+    #contour = plt.contour(colormap)
+    #plt.get_cmap('inferno')
+
+    #NUMPY TRANSFORM
+    colormap = np.matrix.transpose(np.array(colormap))
+    pcm = plt.pcolormesh(colormap)#, color=(0.1,0.1,0.1))
+    #pcm = plt.pcolormesh(rot+base)
+    #pcm.set_color(((0.8,0.1,0.1), (0.1,0.1,0.8)))
+
+    plt.xticks(np.linspace(0, 30, 9), np.linspace(-0.6, 0.6, 9))
+    plt.yticks(np.linspace(0, 45, 9), np.linspace(0.8, 2.5, 9))
+
+    plt.autoscale()
+
+    #im = plt.pcolormesh(np.arange(100).reshape((10, 10)))
+    #plt.colorbar(im)
+    ####plt.axes().
+    #plt.plot(transform=rot)
+    plt.show()
