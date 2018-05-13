760f656d5d7c9cb760db528ed6d342e3f9a247c5
igordecc <Igordecc@yandex.ru>
2018-05-14 00:03:45 +0400

    fix an issues in 1_basics

--- _STUFF/numpy practice/1 basics.py
+++ _STUFF/numpy practice/1 basics.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-thearray = np.ndarray
-thearray.ndim = 3
-thearray.shape = (3, 2, 2)
-assert thearray.size == 3*2*2
+thearray = np.ndarray((3, 2, 2))
+thearray.ndim #= 3
+thearray.shape #= (3, 2, 2)
+assert thearray.size #== 3*2*2
 thearray.dtype = np.float64
 assert thearray.itemsize == 8 #Itemsize count in in bytes. float 64 has itemsize=64/8=8
 
