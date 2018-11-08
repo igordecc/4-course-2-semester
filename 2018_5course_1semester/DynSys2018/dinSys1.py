f85ad20ee1fe02b3ad9976283816d0deda42cee8
The Notebook <Igordecc@yandex.ru>
2018-11-08 13:34:47 +0400

    Add second excample
    Add 2 example - Puancare section with plots.

--- 2018_5course_1semester/DynSys2018/dinSys1.py
+++ 2018_5course_1semester/DynSys2018/dinSys1.py
@@ -2,8 +2,9 @@ import matplotlib
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
 import numpy
+import numpy as np
 
-def ressler(x, y, z, a = .1, b = .2, r = 5.7, h = 0.01 ):     #a = 0.2, b = 0.2, r = 5.7
+def ressler(x, y, z, a = .01, b = .2, r = 5.7, h = 0.01 ):     #a = 0.2, b = 0.2, r = 5.7
     new_x = - y - z
     new_y = x + a*y
     new_z = b + (x-r)*z
@@ -28,15 +29,45 @@ if __name__ == '__main__':
     for i in range(1,evaluateNum):
         xyz[i] = ressler(*xyz[i-1])
 
+    xyz = xyz[(evaluateNum//4):]    # 0. избавиться - переходный процесс
     xyz = numpy.transpose(xyz)
     x = xyz[0]
     y = xyz[1]
     z = xyz[2]
 
+    #плоскость x=2
+    #1. задание плоскости x=2
+
+
+
+    #print(list(enumerate(np.array(x))))
+    def getSliceNums(x, sectionx = 2):
+        np.array(x)
+        np.ndenumerate(x)
+        listt = [i<sectionx  for i in x]
+        pairedListt = [i for i in zip([0] + listt, listt + [0])][1:-2]
+        numberList = [i[0] for i in enumerate(pairedListt) if (i[1] == (False, True)) ] # Puankare sectrion from one side only
+        return numberList
+
+    slicex = []
+    slicey = []
+    slicez = []
+    for i in getSliceNums(x):
+        slicex.append(x[i])
+        slicey.append(y[i])
+        slicez.append(z[i])
+
     ax.plot(x, y, z, label="ressler's attractor")
+    ax.plot(slicex, slicey, slicez, label="ressler's attractor", color = "r")
+    # Y = np.arange(-10, 10, 0.25)
+    # Z = np.arange(-10, 10, 0.25)
+    # Y, Z = np.meshgrid(Y, Z)
+    # X = np.array([2 for i in np.arange(-10, 10, 0.25)])
+    # ax.plot_surface(X,Y,Z, shade=True)
     ax.legend()
+
 
     #Axes3D.plot(xyz)
 
-    print(xyz)
+    #print(xyz)
     pyplot.show()
