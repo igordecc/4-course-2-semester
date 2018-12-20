1478faf8cb26a6cdbf415a868dc787d6497cf9b8
The Notebook <Igordecc@yandex.ru>
2018-12-20 13:35:09 +0400

    Content update
    Made 2.3, 1.4, 1.5. Try to do 2.4 and 2.5.

--- 2018_5course_1semester/DynSys2018/dynSys4and5.py
+++ 2018_5course_1semester/DynSys2018/dynSys4and5.py
@@ -6,23 +6,25 @@ def rk4(t0, t1, y0, F, steps=100):
     h = (t1 - t0) / steps
     t = t0
     y = y0.copy()
+    # for i in range(steps):
+    #     # k1
+    #     k = h * F(t, y) / 2.0
+    #     r = k / 3.0
+    #
+    #     # k2
+    #     k = h * F(t + h / 2.0, y + k) / 2.0
+    #     r += k * (2.0 / 3.0)
+    #
+    #     # k3
+    #     k = h * F(t + h / 2.0, y + k)
+    #     r += k / 3.0
+    #
+    #     # k4
+    #     pass
+    #
+    #     y += r + h * F(t + h, y + k) / 6.0
     for i in range(steps):
-        # k1
-        k = h * F(t, y) / 2.0
-        r = k / 3.0
-
-        # k2
-        k = h * F(t + h / 2.0, y + k) / 2.0
-        r += k * (2.0 / 3.0)
-
-        # k3
-        k = h * F(t + h / 2.0, y + k)
-        r += k / 3.0
-
-        # k4
-        pass
-
-        y += r + h * F(t + h, y + k) / 6.0
+        y += h * F(t, y)
         t += h
     return y
 
@@ -62,31 +64,70 @@ def Lorenz(t, X, sig=10, bet=8/3, r=28):
 
     return res
 
-def ResslerVar(x,y,z, x_, y_, z_, a, b, r):
+# def ResslerVar(x,y,z, x_, y_, z_, a, b, r):
+#
+#     return (
+#         - y_ - z_,
+#         x_ + a*y_,
+#         x_*z + x*z_ - r*z_
+#     )
+#
+# def Ressler(t, X, a=.25, b=.15, r=2.5):
+#     x, y, z = X[0], X[1], X[2]
+#     res = numpy.empty(shape=(12,), dtype=numpy.float)
+#
+#     res[0] = - y - z
+#
+#     res[1] = x + a*y
+#
+#     res[2] = b + (x-r)*z
+#
+#     #        m
+#     # cij = sum aik*bkj
+#     #       k-1
+#
+#     res[3:6] = ResslerVar(X[0], X[1], X[2], X[3], X[4], X[5], a, b, r)
+#     res[6:9] = ResslerVar(X[0], X[1], X[2], X[6], X[7], X[8], a, b, r)
+#     res[9:12] = ResslerVar(X[0], X[1], X[2], X[9], X[10], X[11], a, b, r)
+#
+#     return res
 
+# def EnnoVan(x,y,z, x_, y_, z_, a, b, r):
+#     return (
+#         - a*2*x*x_ - b*y_,
+#         x_,
+#         0
+#     )
+#
+# def Enno(t, X, a=1, b=.15, r=2.5):
+#     x, y, z = X[0], X[1], X[2]
+#     res = numpy.empty(shape=(12,), dtype=numpy.float)
+#
+#     res[0] = 1 - a*x*x +b*y
+#     res[1] = x
+#     res[2] = 0
+#
+#     res[3:6] = EnnoVan(X[0], X[1], X[2], X[3], X[4], X[5], a, b, r)
+#     res[6:9] = EnnoVan(X[0], X[1], X[2], X[6], X[7], X[8], a, b, r)
+#     res[9:12] = EnnoVan(X[0], X[1], X[2], X[9], X[10], X[11], a, b, r)
+
+def EnnoVan(x,y, x_, y_, a, b):
     return (
-        - y_ - z_,
-        x_ + a*y_,
-        x_*z + x*z_ - r*z_
+        - a*2*x*x_ + b*y_,
+        x_
     )
 
-def Ressler(t, X, a=.25, b=.15, r=2.5):
-    x, y, z = X[0], X[1], X[2]
-    res = numpy.empty(shape=(12,), dtype=numpy.float)
+def Enno(t, X, a=0.2, b=.2):
+    x, y = X[0], X[1]
+    res = numpy.empty(shape=(6,), dtype=numpy.float)
 
-    res[0] = - y - z
+    res[0] = 1 - a*x*x +b*y
+    res[1] = x
 
-    res[1] = x + a*y
+    #print(res)
+    res[2:4] = EnnoVan(X[0], X[1], X[2], X[3], a, b)
+    res[4:6] = EnnoVan(X[0], X[1], X[4], X[5], a, b)
 
-    res[2] = b + (x-r)*z
-
-    #        m
-    # cij = sum aik*bkj
-    #       k-1
-
-    res[3:6] = ResslerVar(X[0], X[1], X[2], X[3], X[4], X[5], a, b, r)
-    res[6:9] = ResslerVar(X[0], X[1], X[2], X[6], X[7], X[8], a, b, r)
-    res[9:12] = ResslerVar(X[0], X[1], X[2], X[9], X[10], X[11], a, b, r)
 
     return res
 
@@ -131,7 +172,10 @@ def lyapunov(n, fn, tStart, tStep, tEnd, y0):
     return series
 
 if __name__ == '__main__':
-    series = lyapunov(3, Ressler, 0, 1, 150, (1,2,3))
+    # series = lyapunov(3, Ressler, 0, 1, 150, (1,2,3))
+    # print(series[-1])
+
+    series = lyapunov(2, Enno, 0, 1, 150, (1, 2))
     print(series[-1])
     """
     tCurent, 1, 2, 3-q,
