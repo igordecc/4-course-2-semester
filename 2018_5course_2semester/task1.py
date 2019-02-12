b9bedc1b642810fed4a3492abeccccf670e76f0c
igordecc <Igordecc@yandex.ru>
2019-02-12 12:05:11 +0400

    Added universal scale
    But need to do from that custom scale for all Lambda function.

--- 2018_5course_2semester/task1.py
+++ 2018_5course_2semester/task1.py
@@ -1,4 +1,5 @@
 import numpy
+import matplotlib.pyplot
 
 
 def logistic_map(x, _lambda):
@@ -19,7 +20,12 @@ def logistic_map(x, _lambda):
 # - repeat for all others _lambdas in interval [0;5]
 
 
-def iterate(_lambda, lmapfn, times, delta, x0):
+def iterate(_lambda,
+            lmapfn,
+            times,
+            delta,
+            x0
+            ):
 
     x = x0
     for i in range(times-delta):    #sjould be: delta < times
@@ -43,12 +49,39 @@ def iterate(_lambda, lmapfn, times, delta, x0):
 # unit tests - tests of each single element of our program - CHECK
 # integration tests - global test of 1 feature from _todo - will be at the end
 
-def allLambda(lmin, lmax, ld, *args):
+def allLambda(lmin,
+              lmax,
+              ld,
+              *args
+              ):
     diagramList = []
     _lambdaRow = numpy.arange(lmin, lmax, ld)
     for _lambda in _lambdaRow:
         diagramList.append(iterate(_lambda, *args))
     return _lambdaRow, diagramList
+
+def scale(x_scale_point,
+          y_scale_point,
+          x_scale_coefficient,
+          y_scale_coefficient,
+          xmin,
+          xmax,
+          ymin,
+          ymax
+          ):
+
+    # nxmin, nxmax = xmin * x_scale_coefficient, xmax * x_scale_coefficient
+    # nymin, nymax = ymin * y_scale_coefficient, ymax * y_scale_coefficient
+
+    x_proportions = x_scale_point -  xmin, xmax - x_scale_point
+    n_x_proportions = [ i * x_scale_coefficient for i in x_proportions ]
+    n_xmin, n_xmax = x_scale_point - n_x_proportions[0], x_scale_point + n_x_proportions[1]
+
+    y_proportions = y_scale_point - ymin, ymax - y_scale_point
+    n_y_proportions = [i * y_scale_coefficient for i in y_proportions]
+    n_ymin, n_ymax = y_scale_point - n_y_proportions[0], y_scale_point + n_y_proportions[1]
+
+    return matplotlib.pyplot.axis(n_xmin, n_xmax, n_ymin, n_ymax)
 
 if __name__ == '__main__':
     times = 1000
@@ -56,7 +89,7 @@ if __name__ == '__main__':
     x0 = 0.1
     _lambda = 1.4
     x_array = iterate(_lambda, logistic_map, times, delta, x0)
-    import matplotlib.pyplot
+
     #matplotlib.pyplot.plot(x_array, '.')
     #matplotlib.pyplot.grid()
     #matplotlib.pyplot.show()
@@ -68,4 +101,22 @@ if __name__ == '__main__':
     matplotlib.pyplot.grid()
     matplotlib.pyplot.show()
 
-    #come up with how scalle graphic around Critical point
+
+    # TODO come up with how scalle graphic around Critical point
+    # TODO you have done universal scale, but it will better to make scale function wich is including allLambda() function custom scale
+    xmin = .5
+    xmax = 1.8
+    ymin = -1
+    ymax = 1
+    x_scale_point = 1.4
+    y_scale_point = 0
+    x_scale_coefficient = 1
+    y_scale_coefficient = 0.5
+    # scaling point
+    # x_scale_point = 1.1
+    # y_scale_point = DON'T SCALE
+
+    #
+
+
+
