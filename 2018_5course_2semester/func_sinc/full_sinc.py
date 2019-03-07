0e4342bfd7859d682ba0facf85bbeeaa6f7f443a
igordecc <Igordecc@yandex.ru>
2019-03-08 00:19:35 +0400

    Update task 1.1 sync labs
    Done. Made with functional idioms.

--- 2018_5course_2semester/func_sinc/full_sinc.py
+++ 2018_5course_2semester/func_sinc/full_sinc.py
@@ -2,6 +2,8 @@
 # one direction coupled ressler
 # be-coupled ressler oscillator
 import numpy
+from functools import reduce
+import matplotlib.pyplot as plt
 
 def bec_ressler(state, alienstate, params):
     x, y, z = state
@@ -12,19 +14,38 @@ def bec_ressler(state, alienstate, params):
     newstate = [dxdt, dydt, dzdt]
     return newstate
 
+
+def norm(vector):
+    norm_value = (reduce(lambda a, x: a + x**2, vector, 0))**0.5
+    return norm_value
+
+def euler_method(state, increment, dt):
+    new_state = list(map(lambda x: x[0] + x[1] * dt, zip(state, increment)))
+    return new_state
+
 def update_state(state_d, params):
     state_osc1 = state_d["osc1"][-1]
     state_osc2 = state_d["osc2"][-1]
-    d_dt_osc1 = bec_ressler(state_osc1, state_osc2, params["osc1"])
-    buff = zip(state_osc1, d_dt_osc1)
-    new_state_osc1 = list(map(lambda x: x[0] + x[1] * params["dt"], buff))
+    # print("state_osc1 ", state_osc1)
+    # print("state_osc2 ", state_osc2)
+    increment_osc1 = bec_ressler(state_osc1, state_osc2, params["osc1"])
+    increment_osc2 = bec_ressler(state_osc2, state_osc1, params["osc2"])
+    # print("increment_osc1", increment_osc1)
+    # print("increment_osc2", increment_osc2)
+    dt = params["dt"]
 
-    d_dt_osc2 = bec_ressler(state_osc2, state_osc1, params["osc2"])
-    buff = zip(state_osc2, d_dt_osc2)
-    new_state_osc2 = list(map(lambda x: x[0] + x[1] * params["dt"], buff))
+    new_state_osc1 = euler_method(state_osc1, increment_osc1, dt)
+    new_state_osc2 = euler_method(state_osc2, increment_osc2, dt)
+
+    if len(state_d["savedtime"])>params["startfrom"]:
+        vector_difference = map(lambda x: x[0] - x[1], zip(increment_osc1, increment_osc2))
+        e_error_integral = euler_method([state_d["e_error_norm"][-1]], [norm(vector_difference)], dt)[0]
+        state_d["e_error_norm"].append(e_error_integral)
+        # print(state_d["e_error_norm"][-1] - state_d["e_error_norm"][-2])
 
     state_d["osc1"].append(new_state_osc1)
     state_d["osc2"].append(new_state_osc2)
+
     state_d["savedtime"].append(state_d["time"].pop(0))
 
 def make_timestep(state_d, params):
@@ -39,17 +60,18 @@ if __name__ == '__main__':
             "a": 0.15,
             "p": 0.2,
             "c": 10,
-            "w": 0.89,  # change here [0.89 - 1.01]
+            "w": 0.8,  # change here [0.89 - 1.01]
             "E": 0.1,
         },
         "osc2": {
             "a": 0.15,
             "p": 0.2,
             "c": 10,
-            "w": 0.95,  # const
+            "w": 0.8,  # const
             "E": 0.1,
         },
         "dt": 0.1,
+        "startfrom": 500
     }
 
 
@@ -58,18 +80,21 @@ if __name__ == '__main__':
         "time": [t for t in numpy.arange(0, 100.0001, params["dt"])],
         "savedtime": [],
         "osc1": [[0.1, 0.1, 0.1], ],
-        "osc2": [[0.2, 0.2, 0.2], ]
+        "osc2": [[0.1, 0.1, 0.1], ],
+        "e_error_norm": [0, ]
     }
 
     make_timestep(state_d, params)
 
-    import matplotlib.pyplot as plt
     # map it
-    starfrom = 500
-    x_osc1 = list(map(lambda x: x[0], state_d["osc1"][starfrom:]))
-    x_osc2 = list(map(lambda x: x[0], state_d["osc2"][starfrom:]))
 
+    x_osc1 = list(map(lambda x: x[0], state_d["osc1"][params["startfrom"]:]))
+    x_osc2 = list(map(lambda x: x[0], state_d["osc2"][params["startfrom"]:]))
     plt.plot(x_osc1, x_osc2, "r.")
     plt.grid()
     plt.show()
-    print(x_osc1)
+
+    def e_error():
+        return state_d["e_error_norm"][-1] / (state_d["savedtime"][-1] - state_d["savedtime"][params["startfrom"]])
+
+    print("e_error: ", e_error())
