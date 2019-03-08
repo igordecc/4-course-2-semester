f47ab29a2a6ba1f369bc057bfae6422405b048a4
The Notebook <Igordecc@yandex.ru>
2019-03-08 14:29:53 +0400

    sinc labs update.
    Add e_error calculus. Steel need to rework it.

--- 2018_5course_2semester/func_sinc/full_sinc.py
+++ 2018_5course_2semester/func_sinc/full_sinc.py
@@ -26,6 +26,9 @@ def euler_method(state, increment, dt):
     new_state = list(map(lambda x: x[0] + x[1] * dt, zip(state, increment)))
     return new_state
 
+def e_error(state_d, params):
+    return sum(state_d["e_error_norm"]) / len(state_d["e_error_norm"])/(state_d["savedtime"][-1] - state_d["savedtime"][params["startfrom"]])
+
 def update_state(state_d, params):
     state_osc1 = state_d["osc1"][-1]
     state_osc2 = state_d["osc2"][-1]
@@ -43,7 +46,7 @@ def update_state(state_d, params):
     if (len(state_d["savedtime"])>params["startfrom"]) and abs(increment_osc1[0]) < 2**33:
         vector_difference = list(map(lambda x: x[0] - x[1], zip(increment_osc1, increment_osc2)))
         e_error_integral = euler_method([state_d["e_error_norm"][-1]], [norm(vector_difference)], dt)[0]
-        e_error_sum = sum([state_d["e_error_norm"][-1], norm(vector_difference)])       # backing variant, in case e_error_integral is not right thing - this thing is not right too.
+        # e_error_sum = norm(vector_difference)]       # backing variant, in case e_error_integral is not right thing - this thing is not right too.
         state_d["e_error_norm"].append(e_error_integral)
         # print(state_d["e_error_norm"][-1] - state_d["e_error_norm"][-2]) #TESTs
 
@@ -65,20 +68,18 @@ if __name__ == '__main__':
             "p": 0.2,
             "c": 10,
             "w": 0.81,  # change here [0.89 - 1.01]
-            "E": 1.1,
+            "E": 1.0,
         },
         "osc2": {
             "a": 0.15,
             "p": 0.2,
             "c": 10,
             "w": 0.8,  # const
-            "E": 1.4,
+            "E": 1.0,
         },
         "dt": 0.1,
         "startfrom": 500
     }
-
-
 
     state_d = {
         "time": [t for t in numpy.arange(0, 100.0001, params["dt"])],
@@ -87,19 +88,34 @@ if __name__ == '__main__':
         "osc2": [[0.1, 0.1, 0.1], ],
         "e_error_norm": [0, ]
     }
+    state_d_default = state_d.copy()
 
-    make_timestep(state_d, params)
+    def part1_x1fromx2():
+        make_timestep(state_d, params)
 
-    x_osc1 = list(map(lambda x: x[0], state_d["osc1"][params["startfrom"]:]))
-    x_osc2 = list(map(lambda x: x[0], state_d["osc2"][params["startfrom"]:]))
-    plt.plot(x_osc1, x_osc2, "r.")
-    plt.xlim(-15,20)
-    plt.ylim(-15,20)
-    plt.grid()
-    plt.show()
+        x_osc1 = list(map(lambda x: x[0], state_d["osc1"][params["startfrom"]:]))
+        x_osc2 = list(map(lambda x: x[0], state_d["osc2"][params["startfrom"]:]))
+        plt.plot(x_osc1, x_osc2, "r.")
+        plt.xlim(-15,20)
+        plt.ylim(-15,20)
+        plt.grid()
+        plt.show()
 
-    def e_error():
-        return state_d["e_error_norm"][-1] / (state_d["savedtime"][-1] - state_d["savedtime"][params["startfrom"]])
+        # TODO it's calculating now, but it's not Average. 1. Try make it average, by getting sum instead of integral
+        print("e_error: ", e_error(state_d, params))
 
-    # TODO it's calculating now, but it's not Average. 1. Try make it average, by getting sum instead of integral
-    print("e_error: ", e_error())
+    part1_x1fromx2()
+    def part2_efromE():
+        E_osc1 = [i for i in numpy.arange(0, 2.5, 0.05)]
+        E_osc2 = [i for i in numpy.arange(0, 2.5, 0.05)]
+        e_list = []
+        for E1, E2 in zip(E_osc1, E_osc2):
+            state_d = state_d_default.copy()
+            params["osc1"]["E"] = E1
+            params["osc2"]["E"] = E2
+
+            make_timestep(state_d, params)
+            e_list.append(e_error(state_d, params))
+            #TODO USE PIPE LINES
+
+        map(make_timestep(state_d, params), state_d)
