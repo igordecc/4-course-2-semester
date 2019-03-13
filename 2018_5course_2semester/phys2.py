228c7b75d3925073b46454e61d9a9ce5209cadd2
The Notebook <Igordecc@yandex.ru>
2019-03-13 21:01:55 +0400

    Phys 2 updated to phys 3 with error
    try calculate the expression or write box counting algorithm in general.

--- 2018_5course_2semester/phys2.py
+++ 2018_5course_2semester/phys2.py
@@ -40,31 +40,60 @@ for n in range(0, len(x_array) - 1):
     plt.vlines(x_array[n], x_array[n], x_array[n + 1], "b")
     plt.hlines(x_array[n + 1], x_array[n], x_array[n + 1], "b")
 
-plt.grid()
-plt.show()
+# plt.grid()
+# plt.show()
 
-#for every dot there wil be a number
-r = 0.5
-n = len(x_array) #количество точек
-def find_D_corr(r=0.5):
-    from functools import reduce
-    count_array = [0 for i in x_array]
-    count_array = map(lambda xi: reduce(lambda a, x: a + (1 if abs(xi-x)<=r else 0),
-                                        x_array, 0),
-                      x_array)
-    count_array = list(count_array)
-    D = sum(count_array)/n**2
-    return D
+def do_D2_corr(x_array):
+    #for every dot there wil be a number
+    r = 0.5
+    n = len(x_array) #количество точек
+    def find_D_corr(r=0.5):
+        from functools import reduce
+        count_array = [0 for i in x_array]
+        count_array = map(lambda xi: reduce(lambda a, x: a + (1 if abs(xi-x)<=r else 0),
+                                            x_array, 0),
+                          x_array)
+        count_array = list(count_array)
+        D = sum(count_array)/n**2
+        return D
 
-r_array = numpy.arange(0,1.5,0.005)
+    r_array = numpy.arange(0, 1.5, 0.005)
 
-C_array = list(map(find_D_corr, r_array))
+    C_array = list(map(find_D_corr, r_array))
 
-D = sum(C_array)/sum(r_array)
-print("D: ", D)
-plt.plot(r_array, C_array, "r.")
-plt.yscale("log")
-plt.xscale("log")
-plt.grid()
-plt.show()
-# plt.clf()
+    D = sum(C_array)/sum(r_array)
+    print("D: ", D)
+    plt.plot(r_array, C_array, "r.")
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.grid()
+    plt.show()
+    # plt.clf()
+
+# do_D2_corr(x_array)
+
+xmin = min(x_array)
+xmax = max(x_array)
+
+box_number = 100
+xdelta = (xmax - xmin)/box_number
+xminmax_array = numpy.arange(xmin, xmax + xdelta, xdelta)
+
+# for i,j in :
+#     if :
+#         count += 1
+#         break
+#
+# map(lambda xi: reduce(lambda a,i,j: a+(1 if (xi>=i)and(xi<=j) else 0),
+#                       zip(xminmax_array[:-1], xminmax_array[1:]), 0),
+#     x_array)
+
+x_box_array = map(lambda xi: list(filter(lambda x: all([(xi>=x[0]),(x<=x[1])]),     # will filter only 1 box for each xi
+                                         zip(xminmax_array[:-1], xminmax_array[1:]))
+                                  ),
+                            x_array)
+
+x_box_array = list(x_box_array)
+print(x_box_array)
+
+box_count_array = numpy.zeros(100)
