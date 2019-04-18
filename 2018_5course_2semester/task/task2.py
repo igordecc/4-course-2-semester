d15759da76be842a0f878e81419231adeee36f9b
The Notebook <Igordecc@yandex.ru>
2019-04-18 16:48:20 +0400

    Add task2 a) fixed

--- 2018_5course_2semester/task/task2.py
+++ 2018_5course_2semester/task/task2.py
@@ -1,6 +1,9 @@
 import sup_task_funcs as stf
-import matplotlib.pyplot
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.widgets as wgt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def plot_xarray():
     # point 1
@@ -10,12 +13,30 @@ def plot_xarray():
     delta = 0
     x_array = stf.iterate(_lambda, stf.logistic_map, k, delta, x0)
 
-    matplotlib.pyplot.plot(x_array, '.')
-    matplotlib.pyplot.grid()
-    matplotlib.pyplot.show()
-    matplotlib.pyplot.clf()
+    # plt.plot(x_array, '.')
+    # plt.grid()
+    # plt.show()
+    # plt.clf()
+    fig = plt.figure()
+    ax = fig.gca()
+    the_plot, = ax.plot(x_array, ".")
+    plt.ylabel("x values")
+    ax.legend()
+    axlambd = plt.axes([0.15, 0.01, 0.65, 0.03], facecolor="b")
+    lambd_slider = wgt.Slider(axlambd, "lambda", 1.748, 1.752, valinit=1.7499, valstep=0.00001, valfmt="%1.5f")
 
+
+    def update_for_slider(val):
+        lambd_local = lambd_slider.val
+        the_plot.set_ydata(stf.iterate(lambd_local, stf.logistic_map, k, delta, x0))
+        fig.canvas.draw_idle()
+
+    lambd_slider.on_changed(update_for_slider)
+
+    plt.show()
     return x_array, k
+
+
 
 def lyapunov_index(fn,
                    x0,
@@ -28,7 +49,7 @@ def lyapunov_index(fn,
     for i in range(nsum):
         if i>delta:
             dfdx = stf.diff(fn, x[i], params, dx)
-            lyap_sum += numpy.log(abs(dfdx))
+            lyap_sum += np.log(abs(dfdx))
         x.append(fn(x[i], params))
     lyap_sum /= (nsum-delta)
     return lyap_sum
@@ -45,20 +66,20 @@ def do_lyapunov_map():
     x0 = 0.1
     nsum = 1000
     #TODO change proximity
-    lrange = numpy.arange(1.35, 1.505, 0.0001)     # can change _lambda here
+    lrange = np.arange(1.35, 1.505, 0.0001)     # can change _lambda here
     lindex = [lyapunov_index(stf.logistic_map, x0, _lambda, nsum) for _lambda in lrange]
-    zeros = numpy.zeros(len(lrange))
-    matplotlib.pyplot.plot(lrange, lindex)
-    matplotlib.pyplot.plot(lrange, zeros)   # horizontal line
-    matplotlib.pyplot.grid()
-    matplotlib.pyplot.show()
-    matplotlib.pyplot.clf()
+    zeros = np.zeros(len(lrange))
+    plt.plot(lrange, lindex)
+    plt.plot(lrange, zeros)   # horizontal line
+    plt.grid()
+    plt.show()
+    plt.clf()
 
 def do_count_laminar():
 
     # x values
     x_array, k = plot_xarray()
-    dimension = int(numpy.log10(k))
+    dimension = int(np.log10(k))
     print(dimension)
 
     xmax = round(max(x_array), dimension)
@@ -68,9 +89,9 @@ def do_count_laminar():
     dx = round(0.1**dimension, dimension)
     print(dx)
     # SALVATION *10**3, take int() from them, save in the list
-    xline = [round(i, dimension) for i in  numpy.arange(xmin, xmax, dx)]
+    xline = [round(i, dimension) for i in  np.arange(xmin, xmax, dx)]
     print(xline)
-    # count_array = numpy.zeros((xline.len(), k))
+    # count_array = np.zeros((xline.len(), k))
 
     # turn_x_array_to_countdict()
     x_dict = dict()
@@ -83,14 +104,14 @@ def do_count_laminar():
     for i in x_array:
         x_dict[round(i, dimension)] += 1
 
-    matplotlib.pyplot.grid()
-    matplotlib.pyplot.plot(x_dict)
-    matplotlib.pyplot.show()
-    matplotlib.pyplot.clf()
+    plt.grid()
+    plt.plot(x_dict)
+    plt.show()
+    plt.clf()
 
 
 if __name__ == '__main__':
-    #plot_xarray()
+    plot_xarray()
     #plot_lyapunov()
-    do_lyapunov_map()
+    #do_lyapunov_map()
     #do_count_laminar()
