000ac82f5d4981b88175f03b3296c6665b97ed2c
igordecc <Igordecc@yandex.ru>
2019-05-18 16:06:55 +0400

    Add plot task3_1 throu test
    Now it's plot haotic and periodic areas picture.
    need to optimise evaluate with OCL
    need to plot mo acurate periodic areas

--- 2018_5course_2semester/task/task3_2.py
+++ 2018_5course_2semester/task/task3_2.py
@@ -12,7 +12,7 @@ import timeit
 from copy import deepcopy
 
 def circular_map_kwargs(x, Omega=0.6066, K=1):
-    result_x = x + Omega +( (K / (2*numpy.pi) * numpy.sin(x%(2*numpy.pi))) ) #RIGHT VERTION
+    result_x = (x + Omega + (K / (2*numpy.pi) * numpy.sin(x*2*numpy.pi)) )%1 #RIGHT VERTION
     return result_x
 
 params = {
@@ -21,11 +21,12 @@ params = {
         "Omega": 0.06066,
         "K": 1,
     },
-    "iter_number": 100,
+    # "iter_number": 100,
     "dt": 0.01,
-    "time_limits": [0, 100, 0.01],
+    "time_limits": [0, 20, 0.01],
     "skip": 1000,
 }
+
 state_d = {
     "x_array": numpy.array( [0.01, ] ),
 }
@@ -47,26 +48,18 @@ def evaluate(state_d, params):#(system, state_d, params):
 
 def detect_periods(state_d, params, precision = 10**-5):
     """
-    precision -> multiplier  
-    state_d * multiplier -> applicable_state_d
-    np.unique(applicable_state_d) -> unique values and number of unique values | for test they should be mach
-    
-    :param state_d: 
-    :param params: 
-    :return: 
-    """
-    """
     detect_periods(with_precision)
     """
     multiplier = precision**-1
-    applicable_x_array = [np.int(i*multiplier) for i in state_d["x_array"]]
-
+    applicable_x_array = (deepcopy(state_d["x_array"])*multiplier).astype(np.int)
     unique_array = np.unique(applicable_x_array)
-    print("unique_array: ",unique_array)
-    print("len of array: ",len(unique_array))
-    print("count of unique elements: ", np.unique(applicable_x_array, return_counts=True)[1])
-    # print(np.unique(applicable_x_array))
 
+    # print("unique_array: ",unique_array)
+    # print("len of array: ",len(unique_array))
+    # print("count of unique elements: ", np.unique(applicable_x_array, return_counts=True)[1])
+
+    period_number = len(unique_array)   # because this is the easiest way of detecting p_number
+    return period_number
 
 def compute_parameter_map(state_d, params):
     """
@@ -77,80 +70,33 @@ def compute_parameter_map(state_d, params):
     :return:
     """
 
-    #Omega = 0.60666666
-    Omega = 0.6
-    K = 1
-    x = 0
-    iter_number = 100
+    # Omega = 0.60666666 # K = 1
+    omega_array = np.arange(0, 1, 0.01) # [minimum, maximum, delta]
+    K_array = np.arange(4, 0, -0.05)
 
-    def iter_map(x, Omega, K, iter_number):
-        x_array = [x]
-        for i in range(iter_number):
-            x_array.append( circular_map(x_array[i], Omega, K) )
-        return x_array
+    # K_omega_list = np.array([ [ [K,omega] for K in K_array] for omega in omega_array ])
+    # print(K_omega_list)
+    params_list = []
+    state_d_list = []
+    for omega in omega_array:
+        for K in K_array:
+            params["kwargs"]["Omega"] = omega
+            params["kwargs"]["K"] = K
+            params_list.append(deepcopy(params))
+            state_d_list.append(deepcopy(state_d))
 
-    fig, ax = matplotlib.pyplot.subplots()
+    def make_in_parallel(state_d, params):
+        state_d, params = evaluate(state_d, params)
+        period_number = detect_periods(state_d, params)
+        return period_number
 
-    x_array = iter_map(x, Omega, K, iter_number)
-
-    #my_plot, = matplotlib.pyplot.plot(x_array[:-1],x_array[1:])
-    my_plot, = matplotlib.pyplot.plot(x_array)
-    slider_ax1 = matplotlib.pyplot.axes([0.25, 0.1, 0.65, 0.03])
-    slider_ax2 = matplotlib.pyplot.axes([0.25, 0.15, 0.65, 0.03])
-    slider_Omega = matplotlib.widgets.Slider(slider_ax1, "Omega", 0.1, 5, valinit=Omega) #, valstep=0.1)
-    slider_K = matplotlib.widgets.Slider(slider_ax2, "K", 0.1, 5, valinit=K) #, valstep=0.1) Does not exist anymore?
-
-
-    def update(val):
-        Omega = slider_Omega.val
-        K = slider_K.val
-        x_array = iter_map(x, Omega, K, iter_number)
-        my_plot.set_ydata(x_array)
-        # my_plot.set_xdata(x_array[:-1])
-        # my_plot.set_ydata(x_array[1:])
-
-
-
-        fig.canvas.draw_idle()
-
-    slider_Omega.on_changed(update)
-    slider_K.on_changed(update)
-
-    matplotlib.pyplot.show()
-
-
-
-
-
-# ================time test!====
-
-# def test_all_units():
-#     def test_list_append():
-#         evaluate(state_d, params)#(circular_map_kwargs, deepcopy(state_d), deepcopy(params))
-#
-#     print(timeit.timeit(test_list_append, number=100)/100, " - list append")
-
-    # def detect_periods(state_d, params, precision=10 ** -5):
-    #     """
-    #     precision -> multiplier
-    #     state_d * multiplier -> applicable_state_d
-    #     np.unique(applicable_state_d) -> unique values and number of unique values | for test they should be mach
-    #
-    #     :param state_d:
-    #     :param params:
-    #     :return:
-    #     """
-    #     """
-    #     detect_periods(with_precision)
-    #     """
-    #     multiplier = precision ** -1
-    #     applicable_x_array = [np.int(i * multiplier) for i in state_d["x_array"]]
-    #
-    #     unique_array = np.unique(applicable_x_array)
-    #     print("unique_array: ", unique_array)
-    #     print("len of array: ", len(unique_array))
-    #     print("count of unique elements: ", np.unique(applicable_x_array, return_counts=True)[1])
-    #     # print(np.unique(applicable_x_array))
+    # periods_array = np.array(list( map(make_in_parallel, state_d_list, params_list) )).reshape(len(K_array), len(omega_array))
+    periods_array = np.array(list( map(make_in_parallel, state_d_list, params_list) )).reshape(len(omega_array), len(K_array))
+    periods_array = np.transpose(periods_array)
+    print("done mapping!")
+    # plt plotting============ change to something more sensible or comfy
+    plt.matshow(periods_array)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -158,11 +104,9 @@ if __name__ == '__main__':
     PLAN
     deepcopy state_d params
     pipe line:
-        evaluate
-        detect_priods
-        comp[ute_parameter_map
+        evaluate - to get x_array
+        detect_priods - to determin number of periods
+        compute_parameter_map - do again and again previous points to get a map 
     """
 
-    state_d, params = evaluate(deepcopy(state_d), deepcopy(params))
-    detect_periods(state_d, params)
 
